83040b6703303467abd4abd99cd540f817cbfbc2
Dylan <1422554376@qq.com>
2021-10-26 22:15:02 +0800

    插入SVG

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,6 +1,11 @@
 import styled from 'styled-components'
 import {Link} from 'react-router-dom'
 import React from 'react'
+
+require('./icons/money.svg') 
+require('icons/chart.svg')
+require('icons/tag.svg')
+
 const NavWrapper = styled.nav`
 border:1px solid blue;
 line-height:24px;
@@ -20,13 +25,21 @@ const Nav =()=>{
         <NavWrapper>
             <ul>
             <li>
-            
+            <svg fill='red' className='icon'>
+                <use xlinkHref='#money' />
+            </svg>
               <Link to="/tags">标签页</Link>
             </li>
             <li>
+            <svg fill='red' className='icon'>
+                <use xlinkHref='#tag' />
+            </svg>
               <Link to="/money">记账页</Link>
             </li>
             <li>
+            <svg fill='red' className='icon'>
+                <use xlinkHref='#chart' />
+            </svg>
               <Link to="/Statistics">统计页</Link>
             </li>
           </ul>
