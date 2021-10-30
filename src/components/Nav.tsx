b58e7db0f23a038ac4abedf46999848b5e03a6f5
Dylan <1422554376@qq.com>
2021-10-30 16:48:07 +0800

    数字面板UI完成

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -9,6 +9,7 @@ const NavWrapper = styled.nav`
 border:1px solid blue;
 line-height:24px;
 box-shadow:0 0 3px rgba(0,0,0,0.25);
+background:white;
 > ul{
   display:flex;
   
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -24,14 +24,103 @@ const TagsSection = styled.section`
   }
 `
 const NotesSection = styled.section`
+  padding:0 16px;
+  font-size:14px;
+  > label{
+    display:flex;
+    align-items: center;
+    > span{margin-right:16px;white-space:nowrap;}
+    > input{display:block;width:100%;height:72px;background:none;border:none;place}
+  }
 `
 
 const CategorySection = styled.section`
-
+font-size: 24px;
+>ul{
+  display:flex;
+  background:#c4c4c4;
+  
+  >li{
+    padding:16px 0;
+    width:50%;
+    text-align: center;
+    position:relative;
+    &.selected::after{
+      display:block;
+      height:3px;
+      content:'';
+      background:#333;
+      position:absolute;
+      bottom:0;
+      width:100%;
+      left:0;
+    }
+  }
+}
 `
 
 const NumberPadSection = styled.section`
-
+  display:flex;
+  flex-direction:column;
+  > .output{background:white;
+    font-size:36px;line-height:72px;
+    text-align:right;
+    padding:0 16px;
+    box-shadow:inset 0 -5px 5px -5px rgba(0,0,0,0.25),
+               inset 0 5px 5px -5px rgba(0,0,0,0.25);
+  }
+  > .pad{
+    
+    flex-wrap: wrap;
+    >button{
+      font-size:18px;
+      float:left;
+      border:none;
+      width:25%;
+      height:64px;
+      &.ok{
+        height:128px;
+        float:right;
+      }
+      &.zero{
+        width:50%;
+      }
+      &:nth-child(1){
+        background:#f2f2f2;
+      }
+      &:nth-child(2),
+      &:nth-child(5)
+      {
+        background:#e0e0e0;
+      }
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9)
+      {
+        background:#d3d3d3;
+      }
+      &:nth-child(4),
+      &:nth-child(7),
+      &:nth-child(10)
+      {
+        background:#c1c1c1;
+      }
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13)
+      {
+        background:#b8b8b8;
+      }
+      &:nth-child(12)
+      {
+        background:#9a9a9a;
+      }
+      &:nth-child(14)
+      {
+        background:#a9a9a9;
+      }
+    }
+  }
 `
 
 
@@ -51,17 +140,20 @@ function Money() {
     <NotesSection>
       <label>
         <span>备注</span>
-        <input type='text'/>
+        <input type='text' placeholder='在这里添加备注'/>
       </label>
     </NotesSection>
      <CategorySection>
       <ul>
-        <li>支出</li>
+        <li className="selected">支出</li>
         <li>收入</li>
       </ul>
      </CategorySection>
      <NumberPadSection>
-      <div>
+       <div className='output'>
+         100
+      </div>
+      <div className='pad clearfix'>
         <button>1</button>
         <button>2</button>
         <button>3</button>
@@ -73,9 +165,9 @@ function Money() {
         <button>7</button>
         <button>8</button>
         <button>9</button>
-        <button>OK</button>
-        <button>0</button>
-        <button>.</button>
+        <button className='ok'>OK</button>
+        <button className='zero'>0</button>
+        <button >.</button>
       </div>
      </NumberPadSection>
     </Layout>
